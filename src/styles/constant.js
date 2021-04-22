6340644ca59cc9513f5b3e1b7eae5a9107d052ec
mmyeon <mindfulyeon@gmail.com>
2021-04-22 14:48:23 +0900

    [style] 컬러 상수 추가

---
+++ src/styles/constant.js
@@ -0,0 +1,10 @@
+export const COLORS = {
+  yellow: "#FFBD12",
+  lightYellow: "#F2C94C",
+  pink: "rgba(255, 138, 187, 0.84)",
+  lightBlue: "#9FD5E3",
+  pastelBlue: "rgba(25, 71, 229, 0.5)",
+  orange: "#F95A2B",
+  lightGreen: "#00C6AE",
+  darkGray: "#333333",
+};
