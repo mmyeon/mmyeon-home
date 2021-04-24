49bbdd40037dc67e2f335e969397c09c28e5f716
mmyeon <mindfulyeon@gmail.com>
2021-04-24 23:02:50 +0900

    [feat] 버튼 컴포넌트 생성

---
+++ src/components/Button.js
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components";
+import { COLORS, FONTWEIGHT } from "../styles/constant";
+
+const ButtonContainer = styled.button`
+  background: ${COLORS.orange};
+  padding: 0.6em 2em;
+  border: none;
+  font-size: 0.75em;
+  color: white;
+  border-radius: 0.6em;
+  margin-top: 0.5em;
+  font-family: "Poppins", sans-serif;
+  font-weight: ${FONTWEIGHT.extraBold};
+`;
+
+const Button = ({ title }) => {
+  return <ButtonContainer>{title}</ButtonContainer>;
+};
+
+export default Button;
