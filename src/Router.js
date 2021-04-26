d25e2967aa8d0747572dd6d8b06e703a58ea3d16
mmyeon <mindfulyeon@gmail.com>
2021-04-26 10:47:24 +0900

    [refactor] Route가 아닌 Home 컴포넌트에서 모든 컴포넌트 보여주기

--- src/Router.js
+++ src/Router.js
@@ -1,19 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Contact from "./routes/Contact";
 import Home from "./routes/Home";
-import Learning from "./routes/Learning";
 import PageNotFound from "./routes/PageNotFound";
-import Project from "./routes/Project";
 
 const DefaultRouter = () => {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route path="/learning" component={Learning} />
-        <Route path="/project" component={Project} />
-        <Route path="/contact" component={Contact} />
         <Route component={PageNotFound} />
       </Switch>
     </Router>
