253b19eff51e14774c37409843cdb2ca3b23e33a
mmyeon <mindfulyeon@gmail.com>
2021-04-27 16:18:17 +0900

    [style] 햄버거 버튼 fixed로 position고정

--- src/components/Gnb/HambergurBtn.js
+++ src/components/Gnb/HambergurBtn.js
@@ -1,7 +1,19 @@
 import React from "react";
+import styled from "styled-components";
+
+const HambergurBtnContainer = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 30;
+`;
 
 const HambergurBtn = ({ openNavBar }) => {
-  return <button onClick={openNavBar}>내비게이션아 열려라</button>;
+  return (
+    <HambergurBtnContainer>
+      <button onClick={openNavBar}>내비게이션아 열려라</button>
+    </HambergurBtnContainer>
+  );
 };
 
 export default HambergurBtn;
