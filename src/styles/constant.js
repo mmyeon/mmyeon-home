551e1d12a357cb11620fadd905d2f4239072b207
mmyeon <mindfulyeon@gmail.com>
2021-04-27 13:54:03 +0900

    [style] constant 컬러 추가

--- src/styles/constant.js
+++ src/styles/constant.js
@@ -3,6 +3,7 @@ export const COLORS = {
   lightYellow: "#F2C94C",
   pink: "rgba(255, 138, 187, 0.84)",
   lightBlue: "#9FD5E3",
+  lightSkyBlue: "#E0F4F3",
   pastelBlue: "rgba(25, 71, 229, 0.5)",
   orange: "#F95A2B",
   lightGreen: "#00C6AE",
