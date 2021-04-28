547abec52a96bd244f7eb7923de85b4122e7de11
mmyeon <mindfulyeon@gmail.com>
2021-04-29 01:03:30 +0900

    [feat] Learning 컴포넌트 마크업

--- src/components/Learning.js
+++ src/components/Learning.js
@@ -1,14 +1,81 @@
 import React from "react";
 import styled from "styled-components";
-import { COLORS } from "../styles/constant";
+import { COLORS, FONT_WEIGHT } from "../styles/constant";
+import RoundedRectangle from "./RoundedRectangle";
+import Title from "./Title";
 
 const LearningContainer = styled.div`
   min-height: 100vh;
   background: ${COLORS.pink};
+  word-break: keep-all;
+  text-align: center;
+
+  > .content-container {
+    height: 100vh;
+    margin: 0 auto;
+    padding: 3em 1em;
+    overflow: hidden;
+    position: relative;
+
+    > .avatar-talk {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      padding: 1.5em 0;
+
+      > .avatar-img {
+        width: 4.5em;
+      }
+
+      > .avatar-desc {
+        font-size: 1.07em;
+        letter-spacing: 0.05em;
+        font-weight: ${FONT_WEIGHT.regular};
+        line-height: 1.3;
+        margin-top: 1em;
+      }
+    }
+
+    > .rocket-img {
+      position: relative;
+      width: 19em;
+      top: -1.2em;
+      left: -7em;
+    }
+  }
 `;
 
 const Learning = () => {
-  return <LearningContainer id="learning">Learning</LearningContainer>;
+  const linkList = ["블로그", "읽은 책 목록", "WILT"];
+
+  return (
+    <LearningContainer id="learning">
+      <div className="content-container">
+        <Title text="스몰 스텝을 사랑하는 개발자의 학습방법" />
+        <div className="avatar-talk">
+          <img
+            className="avatar-img"
+            src="/assets/images/avatar.png"
+            alt="avatar"
+          />
+          <span className="avatar-desc">
+            WILT(What I Learnt Today)를 기록하며 매일매일 성장하고 있습니다.
+          </span>
+        </div>
+        <div className="links">
+          {linkList.map((link) => (
+            <RoundedRectangle key={link} title={link} />
+          ))}
+        </div>
+        <img
+          className="rocket-img"
+          src="/assets/images/learning-rocket.png"
+          alt="rocket"
+        />
+      </div>
+    </LearningContainer>
+  );
 };
 
 export default Learning;
