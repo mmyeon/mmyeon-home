e48e04ecae8bb377c63e6e1de554b548863b831a
mmyeon <mindfulyeon@gmail.com>
2021-04-28 15:22:09 +0900

    [add] 미디어쿼리를 위한 break point 추가

---
+++ src/device.js
@@ -0,0 +1,9 @@
+export const DEVICE_WIDTH_MOBILE = 375;
+export const DEVICE_WIDTH_TABLET = 768;
+export const DEVICE_WIDTH_DESKTOP = 1024;
+
+export const device = {
+  mobile: `(min-width: ${DEVICE_WIDTH_MOBILE}px)`,
+  tablet: `(min-width: ${DEVICE_WIDTH_TABLET}px)`,
+  desktop: `(min-width: ${DEVICE_WIDTH_DESKTOP}px)`,
+};
