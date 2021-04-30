ed9a31798f38adf2fb60b6322a932bc14727d95b
mmyeon <mindfulyeon@gmail.com>
2021-04-30 14:33:05 +0900

    [feat] 블로그 링크 버튼인 RoundedRectangle 컴포넌트 마크업

---
+++ src/components/RoundedRectangle.js
@@ -0,0 +1,36 @@
+import React from "react";
+import styled from "styled-components";
+import { COLORS, FONT_WEIGHT } from "../styles/constant";
+
+const Container = styled.a`
+  width: 5.6em;
+  height: 5.8em;
+  background: ${(props) => props.backgroundColor};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 1em;
+  border: 3px solid ${COLORS.darkGray};
+  z-index: 1;
+  font-weight: ${FONT_WEIGHT.semiBold};
+  font-size: 1.12em;
+  padding: 0 0.5em;
+  line-height: 1.2;
+  letter-spacing: 0.7px;
+`;
+
+const RoundedRectangle = ({ title, className, url, backgroundColor }) => {
+  return (
+    <Container
+      role="button"
+      href={url}
+      className={className}
+      target="_blank"
+      backgroundColor={backgroundColor}
+    >
+      {title}
+    </Container>
+  );
+};
+
+export default RoundedRectangle;
