212eb3c48b9ed1c4d6343c694fa47f69dc6d6b9b
mmyeon <mindfulyeon@gmail.com>
2021-05-07 00:01:24 +0900

    [add] 프로젝트에 사용될 컬러 constant 추가

--- src/styles/constant.js
+++ src/styles/constant.js
@@ -5,9 +5,11 @@ export const COLORS = {
   lightBlue: "#9FD5E3",
   lightSkyBlue: "#E0F4F3",
   pastelBlue: "rgba(25, 71, 229, 0.5)",
+  darkBlue: "#4A5DB3",
   orange: "#F95A2B",
   lightGreen: "#00C6AE",
   darkGray: "#333333",
+  white: "#f9fcfb",
 };
 
 export const FONT_WEIGHT = {
