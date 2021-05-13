099d44bef59f45bda59b4e78bda7b79264176077
mmyeon <mindfulyeon@gmail.com>
2021-05-13 16:40:37 +0900

    [refactor] Gnb 컴포넌트의 모듈성을 높임

--- src/App.js
+++ src/App.js
@@ -1,46 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import GlobalStyles from "./styles/GlobalStyles";
 import Router from "./Router";
-import Gnb from "./components/Gnb";
 
 function App() {
-  const [isNavBarOpen, setIsNavBarOpen] = useState(false);
-  const [windowWidth, setWindowWidth] = useState(0);
-
-  useEffect(() => {
-    window.addEventListener("resize", handleResize);
-    setWindowWidth(window.innerWidth);
-    windowWidth >= 768 && setIsNavBarOpen(true);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
   return (
     <>
       <GlobalStyles />
-      <Gnb
-        openNavBar={openNavBar}
-        closeNavBar={closeNavBar}
-        isNavBarOpen={isNavBarOpen}
-        windowInnerWidth={windowWidth}
-      />
       <Router />
     </>
   );
-
-  function openNavBar() {
-    setIsNavBarOpen(true);
-  }
-
-  function closeNavBar() {
-    setIsNavBarOpen(false);
-  }
-
-  function handleResize() {
-    setWindowWidth(window.innerWidth);
-  }
 }
 
 export default App;
--- src/components/Gnb/index.js
+++ src/components/Gnb/index.js
@@ -1,22 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import HambergurBtn from "./HambergurBtn";
 import NavBar from "./NavBar";
 import { DEVICE_WIDTH_TABLET } from "../../device";
 
-const Gnb = ({ isNavBarOpen, openNavBar, closeNavBar, windowInnerWidth }) => {
+const Gnb = () => {
+  const [isNavBarOpen, setIsNavBarOpen] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(0);
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    setWindowWidth(window.innerWidth);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [windowWidth]);
+
   return (
     <div>
-      {windowInnerWidth >= `${DEVICE_WIDTH_TABLET}` ? (
+      {windowWidth >= `${DEVICE_WIDTH_TABLET}` ? (
         <NavBar />
       ) : (
         <>
-          {" "}
           {!isNavBarOpen && <HambergurBtn openNavBar={openNavBar} />}
           {isNavBarOpen && <NavBar closeNavBar={closeNavBar} />}
         </>
       )}
     </div>
   );
+
+  function openNavBar() {
+    setIsNavBarOpen(true);
+  }
+
+  function closeNavBar() {
+    setIsNavBarOpen(false);
+  }
+
+  function handleResize() {
+    setWindowWidth(window.innerWidth);
+  }
 };
 
 export default Gnb;
