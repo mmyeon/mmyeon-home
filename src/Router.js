826794c2ad4efe314946fd51f88971b032ef1ce8
mmyeon <mindfulyeon@gmail.com>
2021-12-16 15:37:12 +0900

    [feat] 이력서페이지로 이동하는 Resume 컴포넌트 추가

--- src/Router.js
+++ src/Router.js
@@ -2,12 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import PageNotFound from "./pages/PageNotFound";
+import Resume from "./pages/Resume";
 
 const DefaultRouter = () => {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/resume" component={Resume} />
+
         <Route component={PageNotFound} />
       </Switch>
     </Router>
---
+++ src/pages/Resume.js
@@ -0,0 +1,12 @@
+import React, { useEffect } from "react";
+
+const Resume = () => {
+  useEffect(() => {
+    window.location.href =
+      "https://ritzy-phalange-090.notion.site/48592773e6cf4d3396d7ce80fa3bf84a";
+  }, []);
+
+  return null;
+};
+
+export default Resume;
